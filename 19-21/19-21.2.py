22ebf2fd7ec847b9f221942c25967a5171246906
Jki748 <94294136+Jki748@users.noreply.github.com>
2023-01-19 22:55:59 +0300

    Add files via upload

---
+++ 19-21/19-21.2.py
@@ -0,0 +1,18 @@
+
+def f(start, second, win_step, curr_step):
+    if start + second >= 41:
+        return win_step % 2 == curr_step % 2
+    if win_step == curr_step:
+        return 0
+    next_step = [f(start + 1, second + 2, win_step, curr_step + 1),
+                 f(start + 2, second + 1, win_step, curr_step + 1),
+                 f(start * 2, second, win_step, curr_step + 1),
+                 f(start, second * 2, win_step, curr_step + 1)]
+    return any(next_step) if (curr_step + 1) % 2 == win_step % 2 else all(next_step)
+
+for s in range(1,32):
+    for step in range(1, 6):
+        if f(s, 8, step, 0):
+            print(s, step)
+
+
---
+++ 19-21/19-21.py
@@ -0,0 +1,22 @@
+"""
++1
+*2
+"""
+
+def f(start, second, win_step, curr_step):
+    if start + second >= 77:
+        return win_step % 2 == curr_step % 2
+    if win_step == curr_step:
+        return 0
+    next_step = [f(start + 1, second, win_step, curr_step + 1),
+                 f(start * 2, second, win_step, curr_step + 1),
+                 f(start, second + 1, win_step, curr_step + 1),
+                 f(start, second * 2, win_step, curr_step + 1)]
+    return any(next_step) if (curr_step + 1) % 2 == win_step % 2 else all(next_step)
+
+for s in range(1,68):
+    for step in range(1, 6):
+        if f(s, 7, step, 0):
+            print(s, step)
+
+
